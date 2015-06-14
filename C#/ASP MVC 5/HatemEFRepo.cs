1e267af900c2bd31b9db275c087287608acb2eb7
Hatem Hamad <hatem.h.hamad@gmail.com>
2015-06-15 00:08:55 +0300

    Create the C# Folder
    Contain two classes , an EF Repo and a very useful HtmlHelper extensions

---
+++ C#/ASP MVC 5/HatemEFRepo.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Blex.Models.Data_Models;
+
+namespace Blex.ModelsHelper
+{
+    public class HatemEFRepo<TObject> :IDisposable where TObject : class 
+    {
+
+        protected BlexModels _context;
+        public HatemEFRepo(BlexModels context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<TObject> GetAll()
+        {
+            return _context.Set<TObject>().AsEnumerable();
+        }
+
+        public async Task<ICollection<TObject>> GetAllAsync()
+        {
+            return await _context.Set<TObject>().ToListAsync();
+        }
+
+        public TObject Get(decimal id)
+        {
+            return _context.Set<TObject>().Find(id);
+        }
+
+        public async Task<TObject> GetAsync(decimal id)
+        {
+            return await _context.Set<TObject>().FindAsync(id);
+        }
+
+        public TObject Find(Expression<Func<TObject, bool>> match)
+        {
+            return _context.Set<TObject>().SingleOrDefault(match);
+        }
+
+        public async Task<TObject> FindAsync(Expression<Func<TObject, bool>> match)
+        {
+            return await _context.Set<TObject>().SingleOrDefaultAsync(match);
+        }
+
+        public ICollection<TObject> FindAll(Expression<Func<TObject, bool>> match)
+        {
+            return _context.Set<TObject>().Where(match).ToList();
+        }
+
+        public async Task<ICollection<TObject>> FindAllAsync(Expression<Func<TObject, bool>> match)
+        {
+            return await _context.Set<TObject>().Where(match).ToListAsync();
+        }
+
+        public TObject Add(TObject t)
+        {
+            var res =_context.Set<TObject>().Add(t);
+            _context.SaveChanges();
+            return res;
+        }
+
+        public async Task<TObject> AddAsync(TObject t)
+        {
+            _context.Set<TObject>().Add(t);
+            await _context.SaveChangesAsync();
+            return t;
+        }
+
+        public TObject Update(TObject updated, decimal key)
+        {
+            if (updated == null)
+                return null;
+
+            TObject existing = _context.Set<TObject>().Find(key);
+            if (existing != null)
+            {
+                _context.Entry(existing).CurrentValues.SetValues(updated);
+                _context.SaveChanges();
+            }
+            return existing;
+        }
+
+        public async Task<TObject> UpdateAsync(TObject updated, decimal key)
+        {
+            if (updated == null)
+                return null;
+
+            TObject existing = await _context.Set<TObject>().FindAsync(key);
+            if (existing != null)
+            {
+                _context.Entry(existing).CurrentValues.SetValues(updated);
+                await _context.SaveChangesAsync();
+            }
+            return existing;
+        }
+
+        public void Delete(TObject t)
+        {
+            _context.Set<TObject>().Remove(t);
+            _context.SaveChanges();
+        }
+
+        public async Task<int> DeleteAsync(TObject t)
+        {
+            _context.Set<TObject>().Remove(t);
+            return await _context.SaveChangesAsync();
+        }
+
+        public int Count()
+        {
+            return _context.Set<TObject>().Count();
+        }
+        
+        public int Count(Expression<Func<TObject, bool>> match)
+        {
+            return _context.Set<TObject>().Count(match);
+        }
+        public async Task<int> CountAsync()
+        {
+            return await _context.Set<TObject>().CountAsync();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                    // TODO: dispose managed state (managed objects).          
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources. 
+        // ~HatemEFRepo() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ C#/ASP MVC 5/HatemHtmlHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Razor;
+using System.Web.Mvc.Html;
+
+namespace Blex.ModelsHelper
+{
+    public static class HatemHtmlHelper
+    {
+        /// <summary>
+        /// call the Native TextBoxFor method but saving some little work
+        /// This is an Html Helper extinsion method
+        /// </summary>
+        /// <param name="classname">CSS Class Name</param>
+        /// <param name="withPlaceHolder">Is this TextBox have a Place Holder</param>
+        public static MvcHtmlString TextBoxForH<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, string classname, bool withPlaceHolder = true)
+        {
+            object htmlAttributes = new{
+                @class=classname,
+                @placeholder=withPlaceHolder?DisplayNameExtensions.DisplayNameFor(htmlHelper,expression):null
+            };
+            return InputExtensions.TextBoxFor(htmlHelper, expression, htmlAttributes);
+        }
+        /// <summary>
+        /// call the Native TextBoxFor method but saving some little work
+        /// This is an Html Helper extinsion method
+        /// </summary>
+        /// <param name="classname">CSS Class Name</param>
+        /// <param name="withPlaceHolder">Is this TextBox have a Place Holder</param>
+        public static MvcHtmlString PasswordForH<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, string classname, bool withPlaceHolder = true)
+        {
+            object htmlAttributes = new
+            {
+                @class = classname,
+                @placeholder = withPlaceHolder ? DisplayNameExtensions.DisplayNameFor(htmlHelper, expression) : null
+            };
+            return InputExtensions.PasswordFor(htmlHelper, expression, htmlAttributes);
+        }
+
+        //
+        // Summary:
+        //     Returns a radio button input element for each property in the object that is
+        //     represented by the specified expression, using the specified HTML attributes.
+        //     With the proper value selected.
+        //
+        // Parameters:
+        //   htmlHelper:
+        //     The HTML helper instance that this method extends.
+        //
+        //   expression:
+        //     An expression that identifies the object that contains the properties to render.
+        //
+        //   value:
+        //     The value of the selected radio button. and when re-displaying the matched value will be set as "Checked"
+        //
+        //   htmlAttributes:
+        //     An object that contains the HTML attributes to set for the element.
+        //
+        public static MvcHtmlString RadioButtonForH<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression,object expressionValue, object value, string classname =null)
+        {
+            object htmlAttributes;
+            if (expressionValue.Equals(value))
+            {
+                htmlAttributes = new
+                {
+                    @class = classname,
+                    @checked = "checked"
+                };
+            }
+            else
+                htmlAttributes = new
+                {
+                    @class = classname
+                };
+            return InputExtensions.RadioButtonFor(htmlHelper, expression, value, htmlAttributes);
+        }
+
+        public static MvcHtmlString AjaxAntiForgeryForm(this HtmlHelper htmlHelper)
+        {
+            string result = "<form id =\"__AjaxAntiForgeryForm\" hidden =\"hidden\" action =\"#\" method =\"post\">";
+            result += htmlHelper.AntiForgeryToken().ToHtmlString();
+            result += "</form>";
+            return new MvcHtmlString(result);
+        }
+    }
+}
