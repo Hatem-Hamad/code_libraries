8b85ef1eb645d6c39c7760be4330c5b8150f56f0
Hatem Hamad <hatem.h.hamad@gmail.com>
2015-06-14 23:06:15 +0300

    Correcting the Bug of If statement
    the if "success" Request if statement has been modified.

--- JavaScript/AjaxJQueryHelpers.js
+++ JavaScript/AjaxJQueryHelpers.js
@@ -1,5 +1,5 @@
 /*!
- * AjaxJQueryHelpers v0.0.1 (https://github.com/Hatem-Hamad/code_libraries/blob/master/JavaScript/AjaxJQueryHelpers.js)
+ * AjaxJQueryHelpers v0.0.3 (https://github.com/Hatem-Hamad/code_libraries/blob/master/JavaScript/AjaxJQueryHelpers.js)
  * 2015 Hatem Hamad.
  * Github : (https://github.com/Hatem-Hamad)
  * LinkedIn : (https://www.linkedin.com/in/hatemhamad)
@@ -7,29 +7,32 @@
 
 
 //creates then removes a Bootsrap alert div inside a given div where type : warning , success ...
-$.makeAlertMessage = function(div_to_alert_in,type, message) {
-    $(div_to_alert_in).append($("<div class='alert-message alert-" + type + " alert-dismissible fade in' role='alert' data-alert>"
-        + "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"
-        + "<strong>"+type+"</strong><p> " + message + " </p></div>"));
-    $(".alert-message").delay(2000).fadeOut("slow", function () { $(this).remove(); });
+//using a good library for alerts called "alertify"
+$.makeAlertMessage = function (div_to_alert_in, type, message) {
+
+    if (type == 'success')
+        alertify.success("<strong>" + type + "</strong><p> " + message + " </p>");
+    else
+        alertify.error("<strong>" + type + "</strong><p> " + message + " </p>");
+    //$(div_to_alert_in).append($("<div class='alert-message alert-" + type + " alert-dismissible fade in' role='alert' data-alert>"
+    //    + "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"
+    //    + "<strong>" + type + "</strong><p> " + message + " </p></div>"));
+    //$(".alert-message").delay(2000).fadeOut("slow", function () { $(this).remove(); });
 }
 
-//send an ajax request to the given URL ,then excute the onsuccess_func,and alert with result from the server
-$.AJAX_to_action = function (Type, URL, Data, onsuccess_func_takes_dataparam,alert_div) {
+$.AJAX_to_action = function (Type, URL, Data, onsuccess_func_takes_dataparam, alert_div) {
     $.ajax({
         type: Type,
         url: URL,
         data: Data,
         success: function (data) {
             //success
-            if (data.success === undefined || data.success === null)
-            {
+            if ((data.success !== undefined && data.success !== null && data.success) || data.errorMessage === undefined) {
                 $.makeAlertMessage(alert_div, "success", "Done !");
                 onsuccess_func_takes_dataparam(data);
-                
+
             }
-            else
-            {
+            else {
                 $.makeAlertMessage(alert_div, "warning", data.errorMessage);
             }
         },
